3cb82cc571673454331c74b7404b6b17a3afac3b
Ryuuuuki507 <135676479+Ryuuuuki507@users.noreply.github.com>
2024-11-23 17:35:30 -0300

    Add files via upload

---
+++ aula_5.py
@@ -0,0 +1,41 @@
+print ("bemvindo!")
+
+
+verdadeiro = True 
+tipo =type(verdadeiro)
+print (tipo)
+mutiplicacao = 1.1 * 3.4
+print (mutiplicacao)
+divisao = 78/8
+print (divisao)
+subtracao = 10-5
+print (subtracao)
+divisao = 16/4 
+print (divisao)
+multiplicacao = 6 * 6 * 7 * 7 * 8 * 9
+print (multiplicacao)
+numero_int = 10
+print (numero_int ** 4)
+
+n1 = float(input('Digite um número:'))
+n2 = float(input('Digite um número:'))
+
+soma =  n1 + n2
+subtracao = n1 -n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+print(f'soma: {soma} | subtração: {subtracao} | multiplicação: {multiplicacao} | divisão {divisao}   ')
+nome  =  input('Digite seu nome: ')
+idade  =  int(input('Digite sua idade: '))
+email =  input('Digite seu e-mail: ')
+endereco = input('Digite seu endereço')
+
+print('..........................................')
+print('DADOS CADASTRADOS:')
+
+print(f''' 
+NOME: {nome}
+IDADE: {idade}
+E-MAIL:{email}
+ENDEREÇO: {endereco}
+''')
